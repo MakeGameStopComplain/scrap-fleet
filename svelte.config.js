3807f1147b82ac695742fbd442364f5861c1aa82
Yikuan Sun <yikuansun2015@gmail.com>
2024-08-16 14:30:34 -0400

    tab -> spaces
    thank you

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,12 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		paths: {
-			relative: true,
-		},
-	}
+    kit: {
+        adapter: adapter(),
+        paths: {
+            relative: true,
+        },
+    }
 };
 
 export default config;
