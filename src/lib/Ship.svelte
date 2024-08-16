88ee2b044627b7b002af9972f1ceee0f63dbdb44
Yikuan Sun <yikuansun2015@gmail.com>
2024-08-16 15:08:26 -0400

    Create ship
    Insanely scuffed.

---
+++ src/lib/Ship.svelte
@@ -0,0 +1,49 @@
+<script context="module">
+    let current;
+</script>
+
+<script>
+    import { onMount } from "svelte";
+
+    export let body = [
+        [".",".",".",".",".",".","."],
+        [".",".","#","#","#",".","."],
+        [".",".",".","#",".",".","."],
+        [".",".","#","#","#",".","."],
+        [".",".",".","#",".",".","."],
+        [".",".","#","#","#",".","."],
+        [".",".",".",".",".",".","."],
+    ];
+
+    let cellSize = 24;
+
+    let fillMap = {
+        ".": "transparent",
+        "#": "red",
+    };
+
+    export let xPos = 200;
+    export let yPos = 200;
+</script>
+
+<div style:position="absolute"
+    style:top="{yPos}px"
+    style:left="{xPos}px"
+    style:transform="translate(-50%, -50%)">
+    {#each body as row, r}
+        {#each row as cell, c}
+            {@const fill = fillMap[cell]}
+            <div style:position="absolute"
+                style:top="{r * cellSize}px"
+                style:left="{c * cellSize}px"
+                style:width="{cellSize}px"
+                style:height="{cellSize}px"
+                style:background={fill}
+            ></div>
+        {/each}
+    {/each}
+
+</div>
+
+<style>
+</style>
---
+++ src/routes/arena/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { onMount } from "svelte";
+    import Ship from "$lib/Ship.svelte";
+    
+    let world;
+
+    let inputs = {};
+
+    let playerPos = {
+        x: 200, y: 200,
+    };
+
+    function tick() {
+        if (inputs["ArrowUp"]) playerPos.y -= 10;
+        else if (inputs["ArrowDown"]) playerPos.y += 10;
+        if (inputs["ArrowRight"]) playerPos.x += 10;
+        else if (inputs["ArrowLeft"]) playerPos.x -= 10;
+
+        requestAnimationFrame(tick);
+    }
+
+    onMount(() => {
+        window.addEventListener("keydown", (e) => {
+            inputs[e.key] = true;
+        });
+        window.addEventListener("keyup", (e) => {
+            inputs[e.key] = false;
+        });
+        tick();
+    });
+</script>
+
+<div bind:this={world}>
+    <Ship xPos={playerPos.x} yPos={playerPos.y} />
+</div>
+
+<style>
+    :global(body) {
+        margin: none;
+    }
+</style>
