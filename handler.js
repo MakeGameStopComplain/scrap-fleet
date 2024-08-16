ec7c97e3aac29ecfcec9e83542fbb2821aeac4d5
Yikuan Sun <yikuansun2015@gmail.com>
2024-08-16 13:58:44 -0400

    Update handler

--- handler.js
+++ handler.js
@@ -12,6 +12,9 @@ class Handler {
         for (let key in this.entities) {
             let entity = this.entities[key];
             entity.update();
+
+            // If entity inactive, add to entitiesToRemove
+            if (!entity.active) this.entitiesToRemove.push(entity);
         }
 
         // Adds new entities
@@ -33,6 +36,13 @@ class Handler {
 
     removeEntity(entity) {
         this.entitiesToRemove.push(entity);
+    }
+
+    clearEntities() {
+        for (let key in this.entities) {
+            this.entities[key].destroy();
+            delete this.entities[key];
+        }
     }
 
 }
