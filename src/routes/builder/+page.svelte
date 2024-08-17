1a3e216352e779d7f11b3df536bc33ba578d389f
Yikuan Sun <yikuansun2015@gmail.com>
2024-08-17 17:54:33 -0400

    Use table for builder

--- src/routes/builder/+page.svelte
+++ src/routes/builder/+page.svelte
@@ -10,8 +10,27 @@
     "...#...\n" +
     "..###..\n" +
     ".......";
+    let shipArr = [];
+    
+    function getShipArr() {
+        shipArr = [];
+        for (let r of shipStr.split("\n")) {
+            let row = r.split("");
+            shipArr.push(row);
+        }
+        shipArr = shipArr;
+    }
+    function getShipStr() {
+        shipStr = "";
+        let rows = [];
+        for (let row of shipArr) {
+            rows.push(row.join(""));
+        }
+        shipStr = rows.join("\n");
+    }
 
     function saveButton() {
+        getShipStr();
         localStorage.setItem("shipStr", shipStr);
         goto("/");
     }
@@ -20,20 +39,43 @@
         if (localStorage.getItem("shipStr")) {
             shipStr = localStorage.getItem("shipStr");
         }
+        getShipArr();
     });
 </script>
 
-<textarea rows={7} cols={7} bind:value={shipStr}></textarea>
+<table>
+    {#each shipArr as row, r}
+        <tr>
+            {#each row as cell, c}
+                <td>
+                    <input type="text" bind:value={shipArr[r][c]} style:width="50px" />
+                </td>
+            {/each}
+        </tr>
+    {/each}
+</table>
 <br />
 <button on:click={saveButton}>Return to le main menu</button>
 
 <style>
     :global(body) {
         margin: none;
+        background-color: black;
     }
 
     textarea {
         resize: none;
         overflow: hidden;
+    }
+
+    table td {
+        width: 100px;
+        height: 100px;
+        border: 1px solid red;
+    }
+
+    table {
+        border-spacing: 0;
+        border-collapse: collapse;
     }
 </style>
