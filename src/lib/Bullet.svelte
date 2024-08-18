8ebf9e47aafc0bfa9d396257e96d868c5e75e046
Yikuan Sun <yikuansun2015@gmail.com>
2024-08-18 12:32:35 -0400

    Fix laser beams

--- src/lib/Bullet.svelte
+++ src/lib/Bullet.svelte
@@ -4,8 +4,9 @@
 
 <script>
     import { onMount } from "svelte";
+    import bulletSprite from "$lib/ship_assets/laser_bullet.png";
 
-    let radius = 5;
+    //let radius = 5;
 
     export let xPos = 200;
     export let yPos = 200;
@@ -15,10 +16,11 @@
 <div style:position="absolute"
     style:top="{yPos}px"
     style:left="{xPos}px"
-    style:width="{radius * 2}px"
-    style:height="{radius * 2}px"
-    style:transform="translate(-50%, -50%) rotate({angle}deg)"
-    style:background-color="blue">
+    style:width="{20}px"
+    style:height="{10}px"
+    style:transform="translate(-50%, -50%) rotate({90 + angle}deg)"
+    style:background-image="url('{bulletSprite}')"
+    style:background-size="100% 100%">
 </div>
 
 <style>
