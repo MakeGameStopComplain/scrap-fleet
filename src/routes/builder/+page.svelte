3a8b5f66a9565f8361409df5ffce758588aecbfc
Yikuan Sun <yikuansun2015@gmail.com>
2024-08-17 20:45:33 -0400

    Fix builder page layout

--- src/routes/builder/+page.svelte
+++ src/routes/builder/+page.svelte
@@ -70,81 +70,87 @@
     let selectedTile = "B";
 </script>
 
-<table>
-    <tr>
-        {#each Object.keys(inventory) as key}
-            {@const count = inventory[key]}
-            <td style:background-image={fillMap[key]} style:transform="rotate(0)"
-                style:outline={selectedTile == key ? "3px solid red" : ""}
-                on:click={() => { selectedTile = key; }}>
-                <span style:color="white" style:background-color="black">
-                    x{count}
-                </span>
-            </td>
-        {/each}
-    </tr>
-</table>
-<br />
+<div class="centered" style:text-align="center">
 
-<table>
-    {#each shipArr as row, r}
-        <tr>
-            {#each row as cell, c}
-                {@const canPlaceTile = (() => {
-                    if (inventory[selectedTile] <= 0) return false;
-                    if (r == 3 && c == 3) return false;
-                    if (selectedTile == "G") {
-                        if (r < 6 && ["B", "C", "R"].includes(shipArr[r + 1][c])) {
-                            return true;
-                        }
-                    }
-                    else if (selectedTile == "T") {
-                        if (r > 0 && ["B", "C", "R"].includes(shipArr[r - 1][c])) {
-                            return true;
-                        }
-                    }
-                    else {
-                        if (r > 0 && shipArr[r - 1][c] != ".") {
-                            return true;
-                        }
-                        if (c > 0 && shipArr[r][c - 1] != ".") {
-                            return true;
-                        }
-                        if (c < 6 && shipArr[r][c + 1] != ".") {
-                            return true;
-                        }
-                        if (r < 6 && shipArr[r + 1][c] != ".") {
-                            return true;
-                        }
-                    }
-                    return false;
-                })()}
-                <td style:background-image={fillMap[cell]}
-                    style:background-color={canPlaceTile ? "#FF000055" : "#00000000"}
-                    on:click={() => {
-                        if (canPlaceTile) {
-                            if (shipArr[r][c] != ".") {
-                                inventory[shipArr[r][c]]++;
+    <div style="display: inline-block;">
+        <table>
+            <tr>
+                {#each Object.keys(inventory) as key}
+                    {@const count = inventory[key]}
+                    <td style:background-image={fillMap[key]} style:transform="rotate(0)"
+                        style:outline={selectedTile == key ? "3px solid red" : ""}
+                        on:click={() => { selectedTile = key; }}>
+                        <span style:color="white" style:background-color="black">
+                            x{count}
+                        </span>
+                    </td>
+                {/each}
+            </tr>
+        </table>
+    </div>
+    <br /> <br />
+
+    <table>
+        {#each shipArr as row, r}
+            <tr>
+                {#each row as cell, c}
+                    {@const canPlaceTile = (() => {
+                        if (inventory[selectedTile] <= 0) return false;
+                        if (r == 3 && c == 3) return false;
+                        if (selectedTile == "G") {
+                            if (r < 6 && ["B", "C", "R"].includes(shipArr[r + 1][c])) {
+                                return true;
                             }
-                            shipArr[r][c] = selectedTile;
-                            inventory[selectedTile]--;
                         }
-                    }}
-                    on:contextmenu={(e) => {
-                        e.preventDefault();
-                        if ((r != 3 || c != 3) && shipArr[r][c] != ".") {
-                            inventory[shipArr[r][c]]++;
-                            shipArr[r][c] = ".";
+                        else if (selectedTile == "T") {
+                            if (r > 0 && ["B", "C", "R"].includes(shipArr[r - 1][c])) {
+                                return true;
+                            }
                         }
-                    }}>
-                </td>
-            {/each}
-        </tr>
-    {/each}
-</table>
-<br />
-<input type="image" on:click={saveButton}
-    src={startButton} alt="START" style:width="200px" />
+                        else {
+                            if (r > 0 && shipArr[r - 1][c] != ".") {
+                                return true;
+                            }
+                            if (c > 0 && shipArr[r][c - 1] != ".") {
+                                return true;
+                            }
+                            if (c < 6 && shipArr[r][c + 1] != ".") {
+                                return true;
+                            }
+                            if (r < 6 && shipArr[r + 1][c] != ".") {
+                                return true;
+                            }
+                        }
+                        return false;
+                    })()}
+                    <td style:background-image={fillMap[cell]}
+                        style:background-color={canPlaceTile ? "#FF000055" : "#00000000"}
+                        on:click={() => {
+                            if (canPlaceTile) {
+                                if (shipArr[r][c] != ".") {
+                                    inventory[shipArr[r][c]]++;
+                                }
+                                shipArr[r][c] = selectedTile;
+                                inventory[selectedTile]--;
+                            }
+                        }}
+                        on:contextmenu={(e) => {
+                            e.preventDefault();
+                            if ((r != 3 || c != 3) && shipArr[r][c] != ".") {
+                                inventory[shipArr[r][c]]++;
+                                shipArr[r][c] = ".";
+                            }
+                        }}>
+                    </td>
+                {/each}
+            </tr>
+        {/each}
+    </table>
+    <br />
+    <input type="image" on:click={saveButton}
+        src={startButton} alt="START" style:width="200px" />
+
+</div>
 
 <audio autoplay loop>
     <source src={menuMusic} type="audio/wav" />
@@ -163,10 +169,18 @@
         transform: rotate(90deg);
         background-size: contain;
         user-select: none;
+        vertical-align: top;
+        text-align: right;
     }
 
     table {
         border-spacing: 0;
         border-collapse: collapse;
+    }
+
+    .centered {
+        position: absolute;
+        top: 50%; left: 50%;
+        transform: translate(-50%, -50%);
     }
 </style>
